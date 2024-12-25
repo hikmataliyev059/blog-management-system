d054443bc382ce66156b4d76b990ff05761f473b
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-26 01:28:38 +0400

    added DbContext configuration for entity framework and category DbSet

---
+++ BlogApp.DAL/Context/BlogAppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using BlogApp.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApp.DAL.Context;
+
+public class BlogAppDbContext : DbContext
+{
+    public BlogAppDbContext(DbContextOptions<BlogAppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+    }
+}
