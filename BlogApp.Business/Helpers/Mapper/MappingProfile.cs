2032a9d5bc16974ad3441b078cbcc2f1457216b9
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-26 01:34:50 +0400

    mapping profile for AutoMapper configuration

---
+++ BlogApp.Business/Helpers/Mapper/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BlogApp.Business.DTOs.Category;
+using BlogApp.Core.Entities;
+
+namespace BlogApp.Business.Helpers.Mapper;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<GetCategoryDto, Category>().ReverseMap();
+        CreateMap<GetAllCategoryDto, Category>().ReverseMap();
+        CreateMap<AddCategoryDto, Category>().ReverseMap();
+        CreateMap<UpdateCategoryDto, Category>().ReverseMap();
+    }
+}
