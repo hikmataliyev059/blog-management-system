356b7d3be26f1db73a7b2e842d05bb1f273fc41d
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-27 00:22:10 +0400

    configure entity properties for AppUser in the database context

---
+++ BlogApp.DAL/Context/Configurations/UserConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using BlogApp.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BlogApp.DAL.Context.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<AppUser>
+{
+    public void Configure(EntityTypeBuilder<AppUser> builder)
+    {
+        builder.Property(u => u.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+    }
+}
