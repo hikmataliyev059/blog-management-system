9cd80ed6eca37411c94d4a42de3eb57782aa7568
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-27 00:17:45 +0400

    add custom exception for handling duplicate category scenarios

---
+++ BlogApp.Business/Helpers/Exceptions/Category/CategoryAlreadyExistsException.cs
@@ -0,0 +1,12 @@
+﻿namespace BlogApp.Business.Helpers.Exceptions.Category;
+
+public class CategoryAlreadyExistsException : Exception
+{
+    public CategoryAlreadyExistsException() : base("Category already exists")
+    {
+    }
+
+    public CategoryAlreadyExistsException(string? message) : base(message)
+    {
+    }
+}
