5831bc6d664b656d1845f5d7f5873ae71cf81935
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-27 00:26:36 +0400

    register data access layer services in the dependency injection container

---
+++ BlogApp.DAL/DalServiceRegistration.cs
@@ -0,0 +1,13 @@
+﻿using BlogApp.DAL.Repositories.Impl;
+using BlogApp.DAL.Repositories.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlogApp.DAL;
+
+public static class DalServiceRegistration
+{
+    public static void AddDalServices(this IServiceCollection services)
+    {
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+    }
+}
