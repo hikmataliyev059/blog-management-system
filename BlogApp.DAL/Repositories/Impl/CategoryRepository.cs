d6ae6fd3b37167f4c5f0e5384c4b057474e9c51f
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-27 00:25:19 +0400

    implement category repository inheriting from the generic repository

---
+++ BlogApp.DAL/Repositories/Impl/CategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using BlogApp.Core.Entities;
+using BlogApp.DAL.Context;
+using BlogApp.DAL.Repositories.Interfaces;
+
+namespace BlogApp.DAL.Repositories.Impl;
+
+public class CategoryRepository : Repository<Category>, ICategoryRepository
+{
+    public CategoryRepository(BlogAppDbContext context) : base(context)
+    {
+    }
+}
