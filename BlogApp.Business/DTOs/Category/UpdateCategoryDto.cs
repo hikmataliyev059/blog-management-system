f54db4e89fe4462ecd78694552c321c1599fe14c
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-27 00:21:23 +0400

    create DTO and validator for updating categories with validation rules

---
+++ BlogApp.Business/DTOs/Category/UpdateCategoryDto.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace BlogApp.Business.DTOs.Category;
+
+public record UpdateCategoryDto
+{
+    public string Name { get; set; }
+}
+
+public class UpdateCategoryDtoValidator : AbstractValidator<UpdateCategoryDto>
+{
+    public UpdateCategoryDtoValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotNull()
+            .NotEmpty()
+            .MaximumLength(20);
+    }
+}
