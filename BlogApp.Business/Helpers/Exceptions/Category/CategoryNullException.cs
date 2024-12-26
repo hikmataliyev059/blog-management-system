4b8b0141686e759492d4a0b5790e0c76e9e7f5f4
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-27 00:18:32 +0400

    add custom exception for handling null category scenarios

---
+++ BlogApp.Business/Helpers/Exceptions/Category/CategoryNullException.cs
@@ -0,0 +1,12 @@
+﻿namespace BlogApp.Business.Helpers.Exceptions.Category;
+
+public class CategoryNullException : Exception
+{
+    public CategoryNullException() : base("Category not found")
+    {
+    }
+
+    public CategoryNullException(string? message) : base(message)
+    {
+    }
+}
