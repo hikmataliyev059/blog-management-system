c094ce817e9a4e352a6ff637c417e69cb3132ee8
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-27 00:14:21 +0400

    add generic repository implementation for CRUD and soft delete in data access

---
+++ BlogApp.DAL/Repositories/Impl/Repository.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq.Expressions;
+using BlogApp.Core.Entities.Common;
+using BlogApp.DAL.Context;
+using BlogApp.DAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApp.DAL.Repositories.Impl;
+
+public class Repository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity, new()
+{
+    private readonly BlogAppDbContext _context;
+
+    public Repository(BlogAppDbContext context)
+    {
+        _context = context;
+    }
+
+    public DbSet<TEntity> Table => _context.Set<TEntity>();
+
+    public async Task<TEntity?> GetByIdAsync(int id)
+    {
+        return await Table.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id && !x.IsDeleted);
+    }
+
+    public IQueryable<TEntity> GetAllAsync()
+    {
+        return Table.Where(x => !x.IsDeleted).AsQueryable();
+    }
+
+    public async Task AddAsync(TEntity entity)
+    {
+        await Table.AddAsync(entity);
+    }
+
+    public Task UpdateAsync(TEntity entity)
+    {
+        Table.Update(entity);
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(TEntity entity)
+    {
+        Table.Remove(entity);
+        return Task.CompletedTask;
+    }
+
+    public Task SoftDeleteAsync(TEntity entity)
+    {
+        entity.IsDeleted = true;
+        Table.Update(entity);
+        return Task.CompletedTask;
+    }
+
+    public async Task<int> SaveChangesAsync()
+    {
+        return await _context.SaveChangesAsync();
+    }
+
+    public IQueryable<TEntity> FindAllAsync(Expression<Func<TEntity, bool>> expression)
+    {
+        return Table.Where(expression);
+    }
+}
