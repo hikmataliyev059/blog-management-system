7ea24be3205d40b4d7f45e97d601e2b66fc54764
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-27 00:19:05 +0400

    add custom exception for handling negative or zero ID scenarios

---
+++ BlogApp.Business/Helpers/Exceptions/Common/NegativeIdException.cs
@@ -0,0 +1,12 @@
+﻿namespace BlogApp.Business.Helpers.Exceptions.Common;
+
+public class NegativeIdException : Exception
+{
+    public NegativeIdException() : base("Id cannot be zero or negative")
+    {
+    }
+
+    public NegativeIdException(string? message) : base(message)
+    {
+    }
+}
