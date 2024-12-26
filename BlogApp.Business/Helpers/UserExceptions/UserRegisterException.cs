f7ea6eed23041ae04c15f3d58d3f50e50510719c
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-27 00:21:51 +0400

    add custom exception for handling user registration failures

---
+++ BlogApp.Business/Helpers/UserExceptions/UserRegisterException.cs
@@ -0,0 +1,12 @@
+﻿namespace BlogApp.Business.Helpers.UserExceptions;
+
+public class UserRegisterException : Exception
+{
+    public UserRegisterException() : base("Registration Failed")
+    {
+    }
+
+    public UserRegisterException(string? message) : base(message)
+    {
+    }
+}
