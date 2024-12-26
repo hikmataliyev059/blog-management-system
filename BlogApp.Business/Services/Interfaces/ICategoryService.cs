e6d0c435d8caf0591cf19229199057ba542a493f
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-27 00:27:01 +0400

    define category service interface with CRUD operations and soft delete functionality

---
+++ BlogApp.Business/Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using BlogApp.Business.DTOs.Category;
+
+namespace BlogApp.Business.Services.Interfaces;
+
+public interface ICategoryService
+{
+    Task<GetCategoryDto> GetByIdAsync(int id);
+
+    GetAllCategoryDto GetAllAsync(); 
+
+    Task AddAsync(AddCategoryDto addCategoryDto); 
+
+    Task UpdateAsync(int id, UpdateCategoryDto updateCategoryDto); 
+
+    Task DeleteAsync(int id); 
+    
+    Task SoftDeleteAsync(int id);
+}
