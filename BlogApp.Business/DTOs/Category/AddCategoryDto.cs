85e57c671ec574f1edc0e7ab9b28266e344f91cc
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-27 00:16:16 +0400

    create DTO and validator for adding categories with validation rules

---
+++ BlogApp.Business/DTOs/Category/AddCategoryDto.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace BlogApp.Business.DTOs.Category;
+
+public record AddCategoryDto
+{
+    public string Name { get; set; }
+}
+
+public class AddCategoryDtoValidator : AbstractValidator<AddCategoryDto>
+{
+    public AddCategoryDtoValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Name cannot be empty")
+            .MaximumLength(20)
+            .WithMessage("Name cannot exceed 20 characters");
+    }
+}
