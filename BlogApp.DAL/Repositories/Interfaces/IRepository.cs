07b299113dcce53dbc3ce2f8411066c07831ece1
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-27 00:12:26 +0400

    implement generic repository interface for CRUD operations in data access, including soft delete and query capabilities

---
+++ BlogApp.DAL/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq.Expressions;
+using BlogApp.Core.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApp.DAL.Repositories.Interfaces;
+
+public interface IRepository<TEntity> where TEntity : BaseEntity, new()
+{
+    DbSet<TEntity> Table { get; }
+
+    Task<TEntity?> GetByIdAsync(int id);
+
+    IQueryable<TEntity> GetAllAsync();
+
+    Task AddAsync(TEntity entity);
+
+    Task UpdateAsync(TEntity entity);
+
+    Task DeleteAsync(TEntity entity);
+    
+    Task SoftDeleteAsync(TEntity entity);
+
+    Task<int> SaveChangesAsync();
+
+    IQueryable<TEntity> FindAllAsync(Expression<Func<TEntity, bool>> expression);
+}
