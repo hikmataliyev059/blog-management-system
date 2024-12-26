74e0f7f6fe1465ca9e6f24f31275601c982a0daf
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-27 00:17:05 +0400

    register business services and FluentValidation in the dependency injection container

---
+++ BlogApp.Business/BusinessServiceRegistrations.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using BlogApp.Business.Services.Impl;
+using BlogApp.Business.Services.Interfaces;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlogApp.Business;
+
+public static class BusinessServiceRegistrations
+{
+    public static void AddBusinessServices(this IServiceCollection services)
+    {
+        services.AddAutoMapper(typeof(BusinessServiceRegistrations));
+        services.AddScoped<ICategoryService, CategoryService>();
+        services.AddScoped<IUserService, UserService>();
+        services.AddControllers().AddFluentValidation(cfg =>
+            cfg.RegisterValidatorsFromAssembly(Assembly.GetExecutingAssembly()));
+    }
+}
